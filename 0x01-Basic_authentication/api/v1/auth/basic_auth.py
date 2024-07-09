d52e371aa998865cf133ea475fa6f167939cfc5d
duncmv <dtindiwensi@gmail.com>
2024-07-09 14:16:02 +0300

    basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,29 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """basic auth class"""
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """extract_base64_authorization_header method"""
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if authorization_header[:6] != 'Basic ':
+            return None
+        return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """decode_base64_authorization_header method"""
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            import base64
+            message_bytes = base64.b64decode(base64_authorization_header)
+            message = message_bytes.decode('utf-8')
+            return message
+        except Exception:
+            return None
